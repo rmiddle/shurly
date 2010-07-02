58810f7ed8d1b67b9c7e47a57bb27d9cea36ddb0
Jeff Robbins <jeff@lullabot.com>
2010-07-02 14:47:00 -0400

    ShURLy Module initial commit to GitHub. *Mostly* working, with the exception of the current inability to delete links, no built-in views, and a non-working Views field handler

---
+++ README.txt
@@ -0,0 +1,4 @@
+ShURLy is a URL shortening service implemented as a module for Drupal.
+
+It was originally written by Jeff Robbins for Lullabot's "lbt.me" shortening service at http://lbt.me
+
---
+++ shurly.js
@@ -0,0 +1,15 @@
+Drupal.behaviors.shurly = function() {
+  $('#edit-result').focus(function(){
+    $(this).select();
+  });
+  //.data('origBG', $(this).css('backgroundColor'));
+  var clip = new ZeroClipboard.Client();
+  clip.setText($('#edit-result').val());
+  clip.setHandCursor( true );
+  clip.glue('shurly-copy', 'shurly-copy-container');
+  clip.addEventListener( 'onComplete', function() {
+    $('#edit-result').css('backgroundColor', '#FFFF3F').fadeTo(300, .1, function(){
+      $(this).fadeTo(1, 1).css('backgroundColor', '#FFF');
+    });
+  });
+}
